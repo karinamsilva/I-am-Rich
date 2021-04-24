404beedafc798e408837e87e48247635e9353205
Karina Martins <karinamartinssilva@live.com>
2021-04-23 18:46:30 -0700

    Initial Commit

---
+++ I am Rich 2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am Rich 2
+//
+//  Created by user196036 on 4/23/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
